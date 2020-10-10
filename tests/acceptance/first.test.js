cf92b715a864f2290b98e911dbc8b92a8e3b1015
Romain <romainstraet@gmail.com>
2020-10-10 17:06:57 +0200

    Add acceptance tests and install jest

---
+++ tests/acceptance/first.test.js
@@ -0,0 +1,29 @@
+import { Selector } from 'testcafe';
+
+fixture('Website').page('../../src/index.html')
+
+test('On launch"', async browser => {
+    await browser
+    // A user, let's call him James, go on the website
+    // He notices that it already shows 2 albums timelines
+    // One with the Beatles discography and one with the Rolling Stones discography
+    .expect(Selector('[data-timelines="0"]').innerText).contains('The Beatles')
+    .expect(Selector('[data-timelines="1"]').innerText).contains('The Rolling Stones')
+
+    // He notices an horizontal axis with the years
+    // Starting 1 year before the oldest albums of all album displayed
+    // Until 1 year after the latests albums of all albums displayed
+    .expect(Selector('[data-timelines="axis"]').innerText).contains('1962')
+    .expect(Selector('[data-timelines="axis"]').innerText).contains('1986')
+    .expect(Selector('[data-timelines="axis"]').innerText).contains('2021')
+   
+
+    // He also notices that :
+    // - Only The Beatles have released an album in 1963 (Please please me)
+
+    // - Only The Rolling stones have released an album in 1981 (Tattoo you)
+
+    // - The Beatles have released 2 albums in 1969 (Yellow Submarine and Abbey Road)
+
+    // - The Rolling Stones have also released an album in 1969 (Let it bleed)
+})
--- tests/acceptance/first.test.ts
+++
@@ -1,7 +0,0 @@
-import { Selector } from 'testcafe';
-
-fixture('First test - Getting started').page('../../dist/index.html')
-
-test('Should display a good old "Hello World"', async t => {
-    await t.expect(Selector('h1').innerText).eql('Hello World')
-})
